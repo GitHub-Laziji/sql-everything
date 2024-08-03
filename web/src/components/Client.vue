ba0fda8697398da526d1d0e02652af05b844f82e
github-laziji <c_smtp@163.com>
2024-08-04 01:18:49 +0800

    init

--- web/src/components/Client.vue
+++ web/src/components/Client.vue
@@ -4,20 +4,8 @@
             <div style="width: 300px;height: 100%;border-top: 1px solid #CDD0D6;">
                 <ProjectMenu></ProjectMenu>
             </div>
-
             <div style="height: 100%;flex: 1;">
-                <el-tabs type="border-card" style="width: 100%;height: 100%;">
-                    <el-tab-pane label="SQL">
-                        <div style="padding: 10px;">
-                            <el-input v-model="queryForm.form.sql" style="width: 100%;" type="textarea" class="sql-input"
-                                placeholder="SQL" />
-                        </div>
-                        <div style="display: flex;border-top: 1px solid #CDD0D6; width: 100%;padding: 10px;">
-                            <el-button @click="query">查询</el-button>
-                        </div>
-                    </el-tab-pane>
-                    <el-tab-pane label="结果"></el-tab-pane>
-                </el-tabs>
+                <SqlBoard></SqlBoard>
             </div>
         </div>
     </div>
@@ -25,38 +13,22 @@
 
 <script>
 import ProjectMenu from "./layout/ProjectMenu.vue";
+import SqlBoard from "./layout/SqlBoard.vue";
 export default {
     components: {
-        ProjectMenu
+        ProjectMenu,
+        SqlBoard
     },
     data() {
         return {
-            queryForm: {
-                form: {
-                    sql: ""
-                }
-            }
+
         }
     },
     mounted() {
 
     },
     methods: {
-        query() {
-            this.$http.post("/api/query", { sql: this.queryForm.form.sql }).then(res => {
-                console.log(res.data);
-            });
-        }
+
     }
 }
 </script>
-
-<style>
-.sql-input {
-    height: calc(100vh - 114px) !important;
-}
-
-.sql-input textarea {
-    height: calc(100vh - 113px) !important;
-}
-</style>
---
+++ web/src/components/layout/SqlBoard.vue
@@ -0,0 +1,48 @@
+<template>
+    <el-tabs type="border-card" style="width: 100%;height: 100%;">
+        <el-tab-pane label="SQL">
+            <div style="padding: 10px;">
+                <el-input v-model="queryForm.form.sql" style="width: 100%;" type="textarea" class="sql-input"
+                    placeholder="SQL" />
+            </div>
+            <div style="display: flex;border-top: 1px solid #CDD0D6; width: 100%;padding: 10px;">
+                <el-button @click="query">查询</el-button>
+            </div>
+        </el-tab-pane>
+        <el-tab-pane label="结果"></el-tab-pane>
+    </el-tabs>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            queryForm: {
+                form: {
+                    sql: ""
+                }
+            }
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+        query() {
+            this.$http.post("/api/query", { sql: this.queryForm.form.sql }).then(res => {
+                console.log(res.data);
+            });
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.sql-input {
+    height: calc(100vh - 114px) !important;
+
+    /deep/ textarea {
+        height: calc(100vh - 113px) !important;
+    }
+}
+</style>
